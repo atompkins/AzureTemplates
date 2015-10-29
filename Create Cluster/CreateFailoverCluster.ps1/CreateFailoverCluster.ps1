1cf53313ee3d00f6fecc006deca5532bea8b831f
atompkins <atompkins@gmail.com>
2015-10-29 18:35:47 +0000

    init

---
+++ Create Cluster/CreateFailoverCluster.ps1/CreateFailoverCluster.ps1
@@ -0,0 +1,494 @@
+#
+# Copyright="© Microsoft Corporation. All rights reserved."
+#
+
+configuration CreateFailoverCluster
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$SQLServiceCreds,
+
+        [Parameter(Mandatory)]
+        [String]$ClusterName,
+
+        [Parameter(Mandatory)]
+        [String]$SharePath,
+
+        [Parameter(Mandatory)]
+        [String[]]$Nodes,
+
+        [Parameter(Mandatory)]
+        [String]$SqlAlwaysOnAvailabilityGroupName,
+
+        [Parameter(Mandatory)]
+        [String]$SqlAlwaysOnAvailabilityGroupListenerName,
+
+        [UInt32]$SqlAlwaysOnAvailabilityGroupListenerPort=1433,
+
+        [Parameter(Mandatory)]
+        [String]$LBName,
+
+        [Parameter(Mandatory)]
+        [String]$LBAddress,
+
+        [Parameter(Mandatory)]
+        [String]$PrimaryReplica,
+
+        [Parameter(Mandatory)]
+        [String]$SecondaryReplica,
+
+        [Parameter(Mandatory)]
+        [String]$SqlAlwaysOnEndpointName,
+
+        [String]$DNSServerName='dc-pdc',
+
+        [UInt32]$DatabaseEnginePort = 1433,
+
+        [String]$DomainNetbiosName=(Get-NetBIOSName -DomainName $DomainName),
+
+        [String[]]$DatabaseNames,
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+
+    )
+
+    Import-DscResource -ModuleName xComputerManagement, xFailOverCluster,CDisk,xActiveDirectory,XDisk,xSqlPs,xNetworking, xSql, xSQLServer
+    [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($Admincreds.UserName)", $Admincreds.Password)
+    [System.Management.Automation.PSCredential]$DomainFQDNCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    [System.Management.Automation.PSCredential]$SQLCreds = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($SQLServiceCreds.UserName)", $SQLServiceCreds.Password)
+    [string]$LBFQName="${LBName}.${DomainName}"
+    
+    Enable-CredSSPNTLM -DomainName $DomainName
+    
+    WaitForSqlSetup
+    doStorage
+    doPower
+
+    Node localhost
+    {
+
+#        xWaitforDisk Disk2
+#        {
+#             DiskNumber = 2
+#             RetryIntervalSec =$RetryIntervalSec
+#             RetryCount = $RetryCount
+#        }
+
+#        cDiskNoRestart DataDisk
+#        {
+#            DiskNumber = 2
+#            DriveLetter = "F"
+#        }
+
+#        xWaitforDisk Disk3
+#        {
+#             DiskNumber = 3
+#             RetryIntervalSec =$RetryIntervalSec
+#             RetryCount = $RetryCount
+#        }
+
+#        cDiskNoRestart LogDisk
+#        {
+#            DiskNumber = 3
+#            DriveLetter = "G"
+#        }
+
+        WindowsFeature FC
+        {
+            Name = "Failover-Clustering"
+            Ensure = "Present"
+        }
+
+        WindowsFeature FCPS
+        {
+            Name = "RSAT-Clustering-PowerShell"
+            Ensure = "Present"
+        }
+
+        WindowsFeature ADPS
+        {
+            Name = "RSAT-AD-PowerShell"
+            Ensure = "Present"
+        }
+        xWaitForADDomain DscForestWait 
+        { 
+            DomainName = $DomainName 
+            DomainUserCredential= $DomainCreds
+            RetryCount = $RetryCount 
+            RetryIntervalSec = $RetryIntervalSec 
+        }
+        xComputer DomainJoin
+        {
+            Name = $env:COMPUTERNAME
+            DomainName = $DomainName
+            Credential = $DomainCreds
+        }
+        xCluster FailoverCluster
+        {
+            Name = $ClusterName
+            DomainAdministratorCredential = $DomainCreds
+            Nodes = $Nodes
+        }
+
+        xWaitForFileShareWitness WaitForFSW
+        {
+            SharePath = $SharePath
+            DomainAdministratorCredential = $DomainCreds
+        }
+
+        xClusterQuorum FailoverClusterQuorum
+        {
+            Name = $ClusterName
+            SharePath = $SharePath
+            DomainAdministratorCredential = $DomainCreds
+        }
+
+        xFirewall DatabaseEngineFirewallRule
+        {
+            Direction = "Inbound"
+            Name = "SQL-Server-Database-Engine-TCP-In"
+            DisplayName = "SQL Server Database Engine (TCP-In)"
+            Description = "Inbound rule for SQL Server to allow TCP traffic for the Database Engine."
+            DisplayGroup = "SQL Server"
+            State = "Enabled"
+            Access = "Allow"
+            Protocol = "TCP"
+            LocalPort = $DatabaseEnginePort -as [String]
+            Ensure = "Present"
+        }
+
+        xFirewall DatabaseMirroringFirewallRule
+        {
+            Direction = "Inbound"
+            Name = "SQL-Server-Database-Mirroring-TCP-In"
+            DisplayName = "SQL Server Database Mirroring (TCP-In)"
+            Description = "Inbound rule for SQL Server to allow TCP traffic for the Database Mirroring."
+            DisplayGroup = "SQL Server"
+            State = "Enabled"
+            Access = "Allow"
+            Protocol = "TCP"
+            LocalPort = "5022"
+            Ensure = "Present"
+        }
+
+        xFirewall ListenerFirewallRule
+        {
+            Direction = "Inbound"
+            Name = "SQL-Server-Availability-Group-Listener-TCP-In"
+            DisplayName = "SQL Server Availability Group Listener (TCP-In)"
+            Description = "Inbound rule for SQL Server to allow TCP traffic for the Availability Group listener."
+            DisplayGroup = "SQL Server"
+            State = "Enabled"
+            Access = "Allow"
+            Protocol = "TCP"
+            LocalPort = "59999"
+            Ensure = "Present"
+        }
+
+        xSqlLogin AddDomainAdminAccountToSysadminServerRole
+        {
+            Name = $DomainCreds.UserName
+            LoginType = "WindowsUser"
+            ServerRoles = "sysadmin"
+            Enabled = $true
+            Credential = $Admincreds
+        }
+
+        xADUser CreateSqlServerServiceAccount
+        {
+            DomainAdministratorCredential = $DomainCreds
+            DomainName = $DomainName
+            UserName = $SQLServicecreds.UserName
+            Password = $SQLServicecreds
+            Ensure = "Present"
+            DependsOn = "[xSqlLogin]AddDomainAdminAccountToSysadminServerRole"
+        }
+
+        xSqlLogin AddSqlServerServiceAccountToSysadminServerRole
+        {
+            Name = $SQLCreds.UserName
+            LoginType = "WindowsUser"
+            ServerRoles = "sysadmin"
+            Enabled = $true
+            Credential = $Admincreds
+            DependsOn = "[xADUser]CreateSqlServerServiceAccount"
+        }
+
+        xSqlServer ConfigureSqlServerWithAlwaysOn
+        {
+            InstanceName = $env:COMPUTERNAME
+            SqlAdministratorCredential = $Admincreds
+            ServiceCredential = $SQLCreds
+            Hadr = "Enabled"
+            MaxDegreeOfParallelism = 1
+            FilePath = "F:\DATA"
+            LogPath = "G:\LOG"
+            DomainAdministratorCredential = $DomainFQDNCreds
+            DependsOn = "[xSqlLogin]AddSqlServerServiceAccountToSysadminServerRole"
+        }
+
+        xSqlEndpoint SqlAlwaysOnEndpoint
+        {
+            InstanceName = $env:COMPUTERNAME
+            Name = $SqlAlwaysOnEndpointName
+            PortNumber = 5022
+            AllowedUser = $SQLServiceCreds.UserName
+            SqlAdministratorCredential = $SQLCreds
+            DependsOn = "[xSqlServer]ConfigureSqlServerWithAlwaysOn"
+        }
+
+        xSqlServer ConfigureSqlServerSecondaryWithAlwaysOn
+        {
+            InstanceName = $SecondaryReplica
+            SqlAdministratorCredential = $Admincreds
+            Hadr = "Enabled"
+            DomainAdministratorCredential = $DomainFQDNCreds
+        }
+
+        xSqlEndpoint SqlSecondaryAlwaysOnEndpoint
+        {
+            InstanceName = $SecondaryReplica
+            Name = $SqlAlwaysOnEndpointName
+            PortNumber = 5022
+            AllowedUser = $SQLServiceCreds.UserName
+            SqlAdministratorCredential = $SQLCreds
+        }
+        
+        xSqlAvailabilityGroup SqlAG
+        {
+            Name = $SqlAlwaysOnAvailabilityGroupName
+            ClusterName = $ClusterName
+            InstanceName = $env:COMPUTERNAME
+            PortNumber = 5022
+            DomainCredential =$DomainCreds
+            SqlAdministratorCredential = $Admincreds
+        }
+        
+        xSQLAddListenerIPToDNS UpdateDNSServer
+        {      
+            Credential =$DomainCreds
+            LBName=$LBName
+            LBAddress=$LBAddress
+            DomainName=$DomainName 
+            DNSServerName=$DNSServerName 
+        }
+
+        xSqlAvailabilityGroupListener SqlAGListener
+        {
+            Name = $SqlAlwaysOnAvailabilityGroupListenerName
+            AvailabilityGroupName = $SqlAlwaysOnAvailabilityGroupName
+            DomainNameFqdn = $LBFQName
+            ListenerPortNumber = $SqlAlwaysOnAvailabilityGroupListenerPort
+            ProbePortNumber = 59999
+            InstanceName = $env:COMPUTERNAME
+            DomainCredential =$DomainCreds
+            SqlAdministratorCredential = $Admincreds
+            DependsOn = "[xSqlAvailabilityGroup]SqlAG"
+        }
+           
+        xSqlNewAGDatabase SQLAGDatabases
+        {
+            SqlAlwaysOnAvailabilityGroupName = $SqlAlwaysOnAvailabilityGroupName
+            DatabaseNames = $DatabaseNames
+            PrimaryReplica = $PrimaryReplica
+            SecondaryReplica = $SecondaryReplica
+            SqlAdministratorCredential = $SQLCreds
+        }
+
+        xSqlTestAGDatabase SQLAGDatabasesVerification
+        {
+            SqlAlwaysOnAvailabilityGroupName = $SqlAlwaysOnAvailabilityGroupName
+            InstanceName = $env:COMPUTERNAME
+            DomainCredential =$DomainCreds
+            DependsOn = "[xSqlNewAGDatabase]SQLAGDatabases"
+        }
+
+        LocalConfigurationManager 
+        {
+            RebootNodeIfNeeded = $True
+        }
+
+    }
+
+}
+function Update-DNS
+{
+    param(
+        [string]$LBName,
+        [string]$LBAddress,
+        [string]$DomainName
+
+        )
+               
+        $ARecord=Get-DnsServerResourceRecord -Name $LBName -ZoneName $DomainName -ErrorAction SilentlyContinue -RRType A
+        if (-not $Arecord)
+        {
+            Add-DnsServerResourceRecordA -Name $LBName -ZoneName $DomainName -IPv4Address $LBAddress
+        }
+}
+function WaitForSqlSetup
+{
+    # Wait for SQL Server Setup to finish before proceeding.
+    while ($true)
+    {
+        try
+        {
+            Get-ScheduledTaskInfo "\ConfigureSqlImageTasks\RunConfigureImage" -ErrorAction Stop
+            Start-Sleep -Seconds 5
+        }
+        catch
+        {
+            break
+        }
+    }
+}
+function doStorage
+{
+  $disks = Get-PhysicalDisk -CanPool $true
+  $datadisks = $disks | select -First 8
+  $logdisks = $disks | select -Skip 8
+
+
+  $datapool = New-StoragePool `
+      -FriendlyName "DataPool" `
+      -StorageSubsystemFriendlyName "Storage Spaces*" `
+      -PhysicalDisks $datadisks
+
+  $dataVD = $datapool | New-VirtualDisk `
+      -FriendlyName "DataDisk" `
+      -UseMaximumSize `
+      -NumberOfColumns $datadisks.Count `
+      -ResiliencySettingName "Simple" `
+      -ProvisioningType Fixed `
+      -Interleave 65536
+
+  $dataVD | Initialize-Disk `
+      -Confirm:$False `
+      -PassThru | `
+  New-Partition `
+      -DriveLetter F `
+      -UseMaximumSize | `
+  Format-Volume `
+      -FileSystem NTFS `
+      -NewFileSystemLabel "DATA" `
+      -Confirm:$false	`
+      -AllocationUnitSize 65536
+
+
+  $logpool = New-StoragePool `
+      -FriendlyName "LogPool" `
+      -StorageSubsystemFriendlyName "Storage Spaces*" `
+      -PhysicalDisks $logdisks
+
+  $logVD = $logpool | New-VirtualDisk `
+      -FriendlyName "LogDisk" `
+      -UseMaximumSize `
+      -NumberOfColumns $logdisks.Count `
+      -ResiliencySettingName "Simple" `
+      -ProvisioningType Fixed `
+      -Interleave 65536
+
+  $logVD | Initialize-Disk `
+      -Confirm:$False `
+      -PassThru | `
+  New-Partition `
+      -DriveLetter G `
+      -UseMaximumSize | `
+  Format-Volume `
+      -FileSystem NTFS `
+      -NewFileSystemLabel "LOG" `
+      -Confirm:$false	`
+      -AllocationUnitSize 65536
+}
+function doPower
+{
+  Try {
+    $HighPerf = powercfg -l | %{if($_.contains("High performance")) {$_.split()[3]}}
+    $CurrPlan = $(powercfg -getactivescheme).split()[3]
+    if ($CurrPlan -ne $HighPerf) {powercfg -setactive $HighPerf}
+  } Catch {
+    Write-Warning -Message "Unable to set power plan to high performance"
+  }
+}
+function Get-NetBIOSName
+{ 
+    [OutputType([string])]
+    param(
+        [string]$DomainName
+    )
+
+    if ($DomainName.Contains('.')) {
+        $length=$DomainName.IndexOf('.')
+        if ( $length -ge 16) {
+            $length=15
+        }
+        return $DomainName.Substring(0,$length)
+    }
+    else {
+        if ($DomainName.Length -gt 15) {
+            return $DomainName.Substring(0,15)
+        }
+        else {
+            return $DomainName
+        }
+    }
+}
+function Enable-CredSSPNTLM
+{ 
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$DomainName
+    )
+    
+    # This is needed for the case where NTLM authentication is used
+
+    Write-Verbose 'STARTED:Setting up CredSSP for NTLM'
+   
+    Enable-WSManCredSSP -Role client -DelegateComputer localhost, *.$DomainName -Force -ErrorAction SilentlyContinue
+    Enable-WSManCredSSP -Role server -Force -ErrorAction SilentlyContinue
+
+    if(-not (Test-Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -ErrorAction SilentlyContinue))
+    {
+        New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows -Name '\CredentialsDelegation' -ErrorAction SilentlyContinue
+    }
+
+    if( -not (Get-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name 'AllowFreshCredentialsWhenNTLMOnly' -ErrorAction SilentlyContinue))
+    {
+        New-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name 'AllowFreshCredentialsWhenNTLMOnly' -value '1' -PropertyType dword -ErrorAction SilentlyContinue
+    }
+
+    if (-not (Get-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name 'ConcatenateDefaults_AllowFreshNTLMOnly' -ErrorAction SilentlyContinue))
+    {
+        New-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name 'ConcatenateDefaults_AllowFreshNTLMOnly' -value '1' -PropertyType dword -ErrorAction SilentlyContinue
+    }
+
+    if(-not (Test-Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -ErrorAction SilentlyContinue))
+    {
+        New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name 'AllowFreshCredentialsWhenNTLMOnly' -ErrorAction SilentlyContinue
+    }
+
+    if (-not (Get-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -Name '1' -ErrorAction SilentlyContinue))
+    {
+        New-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -Name '1' -value "wsman/$env:COMPUTERNAME" -PropertyType string -ErrorAction SilentlyContinue
+    }
+
+    if (-not (Get-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -Name '2' -ErrorAction SilentlyContinue))
+    {
+        New-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -Name '2' -value "wsman/localhost" -PropertyType string -ErrorAction SilentlyContinue
+    }
+
+    if (-not (Get-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -Name '3' -ErrorAction SilentlyContinue))
+    {
+        New-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -Name '3' -value "wsman/*.$DomainName" -PropertyType string -ErrorAction SilentlyContinue
+    }
+
+    Write-Verbose "DONE:Setting up CredSSP for NTLM"
+}
+
