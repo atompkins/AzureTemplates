7c6022d13d6869be42b2967f65e716544e342539
atompkins <atompkins@gmail.com>
2015-11-24 14:00:08 +0000

    .

---
+++ Simple-Server-domain-join/Configuration/Configuration.ps1
@@ -0,0 +1,44 @@
+﻿configuration DomainJoin 
+{ 
+   param 
+    ( 
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+
+        [Int]$RetryCount = 20,
+        [Int]$RetryIntervalSec = 30
+    ) 
+    
+    Import-DscResource -ModuleName xActiveDirectory, xComputerManagement
+
+    [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+   
+    Node localhost
+    {
+        WindowsFeature ADPowershell
+        {
+            Name = "RSAT-AD-PowerShell"
+            Ensure = "Present"
+        } 
+
+        xWaitForADDomain WaitForDomain 
+        { 
+            DomainName = $DomainName 
+            DomainUserCredential= $Admincreds
+            RetryCount = $RetryCount 
+            RetryIntervalSec = $RetryIntervalSec
+            DependsOn = "[WindowsFeature]ADPowershell" 
+        }
+
+        xComputer DomainJoin
+        {
+            Name = $env:COMPUTERNAME
+            DomainName = $DomainName
+            Credential = $DomainCreds
+            DependsOn = "[xWaitForADDomain]WaitForDomain" 
+        }
+   }
+} 
